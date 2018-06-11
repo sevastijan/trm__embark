fb5dc02d0f9312eb6b389d9cd68a8b3d4f06e930
daniel-gladysz <dgladysz1@gmail.com>
2018-06-11 13:31:01 +0200

    First commit - add fonts, favicon and logo

--- wp-content/themes/embark/templates/homepage.php
+++ wp-content/themes/embark/templates/homepage.php
@@ -11,18 +11,21 @@
 
 <main id="main" class="homepage-template">
 
-	<section class="intro">
+	<section class="intro element-paddings">
 		<div class="container">
 			<div class="row">
-				<div class="col-12">
-					<h1 class="intro__title">Hello World!</h1><!-- /.intro__title -->
+				<div class="col-md-6">
+					<h1 class="intro__title">Grow your business with Quick, Simple and Accurate Data Capture</h1><!-- /.intro__title -->
 					<div class="intro__content content element-small-margin-top">
-						<?php the_content(); ?>
+						<p>Embark is a bespoke user-friendly on boarding process that allows you to increase sales volume, reach a larger audience and enlarge turnover, all with less effort.</p>
 					</div><!-- /.intro__content content element-small-margin-top -->
-				</div><!-- /.col-12 -->
+				</div><!-- /.col-md-6 -->
+				<div class="col-md-6">
+					video
+				</div><!-- /.col-md-6 -->
 			</div><!-- /.row -->
 		</div><!-- /.container -->
-	</section><!-- /.intro -->
+	</section><!-- /.intro element-paddings -->
 
 </main><!-- /#main.homepage-template -->
 
