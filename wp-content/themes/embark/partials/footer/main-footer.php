4b660b89545d961d25c3ce8abc37a70c8d630cb6
daniel-gladysz <dgladysz1@gmail.com>
2018-06-21 15:17:06 +0200

    Add switcher languages

--- wp-content/themes/embark/partials/footer/main-footer.php
+++ wp-content/themes/embark/partials/footer/main-footer.php
@@ -5,39 +5,30 @@
 				<div class="col-12 col-md">
 					<h3 class="main-footer__title">Head Office</h3><!-- /.main-footer__title -->
 					<address class="main-footer__address element-small-margin-top">
-						<p>Kempton House, Kempton Way,<br>PO Box 9562<br>Grantham, Lincolnshire, NG31 0EA</p>
+						<p><?php the_field( 'address', 'options' ); ?></p>
 					</address><!-- /.main-footer__address element-small-margin-top -->
 				</div><!-- /.col-12 col-md -->
 				<div class="col-12 col-md">
 					<h3 class="main-footer__title element-margin-top mt-md-0">Paylink Products</h3><!-- /.main-footer__title element-margin-top mt-md-0 -->
-					<ul class="main-footer__nav list-unstyled element-small-margin-top">
-						<li class="nav-item">
-							<a href="#" class="nav-link">Paylink Embark</a>
-						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">Paylink CPQ</a>
-						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">Paylink Platform</a>
-						</li>
-					</ul><!-- /.main-footer__nav list-unstyled element-small-margin-top -->
+
+					<?php
+					    wp_nav_menu([
+					        'menu'            => 'Footer Navigation - left column',
+					        'theme_location'  => 'footer_navigation_left_column'
+					    ]);
+					?>
+
 				</div><!-- /.col-12 col-md -->
 				<div class="col-12 col-md">
 					<h3 class="main-footer__title element-margin-top mt-md-0">Sector Solutions</h3><!-- /.main-footer__title element-margin-top mt-md-0 -->
-					<ul class="main-footer__nav list-unstyled element-small-margin-top">
-						<li class="nav-item">
-							<a href="#" class="nav-link">Banking</a>
-						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">Insurance</a>
-						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">Lending</a>
-						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">Debt</a>
-						</li>
-					</ul><!-- /.main-footer__nav list-unstyled element-small-margin-top -->
+
+					<?php
+					    wp_nav_menu([
+					        'menu'            => 'Footer Navigation - right column',
+					        'theme_location'  => 'footer_navigation_right_column'
+					    ]);
+					?>
+
 				</div><!-- /.col-12 col-md -->
 				<div class="col-lg-5">
 					<h3 class="main-footer__title element-margin-top mt-lg-0">Subscribe to latest news</h3><!-- /.main-footer__title element-margin-top mt-lg-0 -->
@@ -53,7 +44,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-12">
-						<span class="second-stage__description">Paylink is a trading name of Paylink Solutions Limited. Paylink Solutions Limited is a limited company registered in England with registered number:10318423. Registered address: Kempton House, Kempton Way, PO Box 9562, Grantham, Lincolnshire, NG31 0EA.</span>
+						<span class="second-stage__description"><?php the_field( 'footer_privacy_policy', 'options' ); ?></span>
 					</div><!-- /.col-12 -->
 					<div class="col-12">
 						<?php
--- wp-content/themes/embark/partials/header/navbar.php
+++ wp-content/themes/embark/partials/header/navbar.php
@@ -29,7 +29,43 @@
                 </button>
             </div><!-- /.col col-auto -->
             <div class="col col-auto d-none d-md-flex">
-                <a href="#get-in-touch" class="embark-button embark-button--small embark-button__full-background embark-button__full-background--primary-color mt-0">Get in touch</a>
+                <div class="row no-gutters align-items-center">
+                    <div class="col col-auto">
+                        <a href="#get-in-touch" class="embark-button embark-button--small embark-button__full-background embark-button__full-background--primary-color mt-0">Get in touch</a>
+                    </div><!-- /.col col-auto -->
+
+                    <?php if(get_field( 'show_switcher', 'options' )): ?>
+
+                        <div class="col col-auto">
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+
+                                    <?php $label =  get_field( 'dropdown_label', 'options' ); ?>
+
+                                    <a href="#dropdown" class="nav-link dropdown-toggle" data-toggle="dropdown"><img src="<?= $label['url']; ?>" alt="<?= $label['alt']; ?>" /></a>
+
+                                    <?php if ( have_rows( 'language', 'options' ) ) : ?>
+
+                                        <div class="dropdown-menu dropdown-menu--laguage">
+
+                                            <?php while ( have_rows( 'language', 'options' ) ) : the_row(); ?>
+                                                <?php $flag = get_sub_field( 'flag' ); ?>
+
+                                                <a href="<?php the_sub_field( 'url' ); ?>" class="dropdown-item"><img src="<?= $flag['url']; ?>" alt="<?= $flag['alt']; ?>" class="icon-flag d-inline-block" /> <?php the_sub_field( 'name' ); ?></a>
+
+                                            <?php endwhile; ?>
+
+                                        </div><!-- /.dropdown-menu -->
+
+                                    <?php endif; ?>
+
+                                </li><!-- /.nav-item dropdown -->
+                            </ul><!-- /.navbar-nav -->
+                        </div><!-- /.col col-auto -->
+
+                    <?php endif; ?>
+
+                </div><!-- /.row no-gutters align-items-center -->
             </div><!-- /.col col-auto d-none d-md-flex -->
         </div><!-- /.row align-items-center justify-content-between w-100 no-gutters -->
     </div><!-- /.container -->
